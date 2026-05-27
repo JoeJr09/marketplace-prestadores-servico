3cf0ba146f4d29612b105b6ba686a0b5a278b653
Renan Cesar <renancesar_lima@hotmail.com>
2026-05-27 18:25:51 -0300

    adição de botão

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,10 @@
+import BotaoInicial from "./BotaoInicial";
+
 export default function Header() {
-  return <header className="bg-amber-500">esse é o cabeçalho</header>;
+  return (
+    <header className="bg-amber-500">
+      esse é o cabeçalho
+      <BotaoInicial />
+    </header>
+  );
 }
